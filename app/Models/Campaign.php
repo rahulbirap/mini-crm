269c88bffae2faff95ce66cbf6ebd7561db8a974
Rahul Birap <rahulbirap@gmail.com>
2026-05-18 18:03:24 +0530

    fix(db): update seeders for accurate demo data

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Campaign extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'campaign_name',
         'client_id',
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'client_name',
